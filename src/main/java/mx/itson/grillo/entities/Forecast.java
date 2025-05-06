08d3f9db030f402948afe13f55c8c29e491037d7
alumnog <alumnog@S921-6.itson.edu.mx>
2025-05-06 10:12:27 -0700

    Se crearon el proyecto y se agregaron las clases de entidad y enum

---
+++ src/main/java/mx/itson/grillo/entities/Forecast.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.grillo.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author alumnog
+ */
+public class Forecast {
+    
+    private Date day;
+    private int maxTemperature;
+    private int minTemperature;
+    
+    
+    
+    
+}
---
+++ src/main/java/mx/itson/grillo/enums/WeatherStatus.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package mx.itson.grillo.enums;
+
+/**
+ *
+ * @author alumnog
+ */
+public enum WeatherStatus {
+    
+    SUNNY, SNOW, RAIN
+    
+}
